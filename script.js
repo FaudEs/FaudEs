3c75bbce6bbf275aef187b01d2887b0119cddc60
FaudEs <FaudEs.dev@protonmail.com>
2024-12-09 23:44:45 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const toggleButton = document.getElementById("toggle-theme");
+
+toggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    toggleButton.textContent = document.body.classList.contains("dark-mode")
+        ? "☀️ Mode Clair"
+        : "🌙 Mode Sombre";
+});
